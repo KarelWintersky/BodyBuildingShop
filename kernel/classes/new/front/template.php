ecba1dafae2dcb311b82db96239442969ab6ae71
Anton Vorozheev <anton.vorozheev@gmail.com>
2014-12-19 09:43:33 +0300

    replace links

--- kernel/classes/new/front/template.php
+++ kernel/classes/new/front/template.php
@@ -4,15 +4,18 @@ Class Front_Template{
 	private $registry;
 	
 	private $Front_Template_Vars;
+	private $Front_Template_Links;
 		
 	public function __construct($registry){
 		$this->registry = $registry;
 		
 		$this->Front_Template_Vars = new Front_Template_Vars($this->registry);
+		$this->Front_Template_Links = new Front_Template_Links($this->registry);
 	}	
 	
 	public function do_template($html){
 		$html = $this->Front_Template_Vars->vars_replace($html);
+		$html = $this->Front_Template_Links->do_links($html);
 		
 		return $html;
 	}
---
+++ kernel/classes/new/front/template/links.php
@@ -0,0 +1,61 @@
+<?php
+Class Front_Template_Links{
+
+	private $registry;
+		
+	public function __construct($registry){
+		$this->registry = $registry;
+	}	
+			
+	private function to_lowercase($link){
+		$link = mb_strtolower($link,'utf-8');
+		
+		return $link;
+	}
+	
+	private function replace_domain($link){
+		$domains = array(
+				'new2.bodybuilding-shop.ru',
+				'www.bodybuilding-shop.ru',
+				'bodybuilding-shop.ru',
+				'bodybuilding-shop',
+				);
+		
+		$link = str_replace('http://','',$link);
+		
+		foreach($domains as $d)
+			$link = ltrim($link,$d);
+		
+		if(!$link  || $link=='/') return THIS_URL;
+		
+		return sprintf('%s/%s',
+				THIS_URL,
+				ltrim($link,'/')
+				);
+	}
+	
+	private function do_replace($matches){
+		$link = $matches[1];
+		$link = $this->to_lowercase($link);
+		$link = $this->replace_domain($link);
+		
+		return str_replace(
+				$matches[1],
+				$link,
+				$matches[0]
+				);
+	}
+	
+	public function do_links($html){
+		$reg = '/<a href=\"([^\"]*)\">.*<\/a>/iU';
+		$html = preg_replace_callback(
+				$reg,
+				array($this,'do_replace'),
+				$html
+				);
+		
+		return $html;
+	}
+		
+}
+?>
