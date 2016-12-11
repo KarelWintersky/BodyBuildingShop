a307d3466c6485c761c7db9687a14a2a0678ac15
Anton Vorozheev <mail@avorozheev.com>
2016-12-11 14:19:27 +0300

    1

--- kernel/controllers/order/index.php
+++ kernel/controllers/order/index.php
@@ -36,7 +36,6 @@ Class Controller_Index Extends Controller_Base{
     }
     
     function check(){
-		echo 2;exit();
     	Front_Order_Steps::check_step(4);
     	
     	$this->registry->set('noindex',true);
